36c98857e9c4aa4f5a934295432c6303bcc4f14e
TheLastCD <lambopuggy@gmail.com>
2025-03-27 10:39:55 +0000

    started sketching out the module class, might need to rethink sensor configuration

--- main.py
+++ main.py
@@ -12,13 +12,24 @@ class IO(Enum):
 
 
 class Sensors():
+    """
+        Sensor super class to handle returns of object identity
+
+
+    """
+
     def __init__(self, typeEnum, io, channel):
+        """
+            args:
+                typeEnum:   Enum defining the type of sensor, either digital or analogue
+                io:         define whether sensor is on an input or an output
+                Channel:    which channel is the sensor on, in real world usecase this would likely be a gpio pin or more specific hardware address
+        """
         self.sensorType = typeEnum
         self.IO = io
         self.channel = channel
 
     # Simple Returns for REST like structure
-
     def returnChannel(self):
         return self.channel
 
@@ -46,11 +57,33 @@ class SensorOutput(Sensors):
         super().__init__(typeEnum, io, channel)
 
     def changePsuState(self, on: bool):
+        """
+            args:
+                on: are we setting the sensor on
+        """
         pass
 
     def ReadPowerConsumption(self):
         pass
 
 
+class Module():
+    def __init__(self):
+        pass
+
+    def SensorFactory(self):
+        pass
+
+
 x = SensorInput(SensorType.DIGI, IO.INP, 1)
 print(x.channel)
+
+
+sensoreDict = {
+    Digi0: 0,
+    Digi1: 1,
+    Ana0: 2,
+    Ana1: 3,
+
+
+}
