3e96b7cbc7dc9909895169cd562e3883a7f0890d
TheLastCD <lambopuggy@gmail.com>
2025-03-29 09:54:44 +0000

    rethinking the approach, time for a refactor as well, enums everywhere

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import re
+
 from enum import Enum
 
 
@@ -11,9 +13,35 @@ class IO(Enum):
     OUT = 1
 
 
+class ProtocolCommands(Enum):
+    ECHO = "E"  # Only requires Sequence Number
+    INPUT = "I"  # Read inputs or return current output value on a given output
+    OUTPUT = "O"  # sets the value of a given output
+    PUS = "P"  # Only requires sequence number
+
+
+class ProtocolReturnCode(Enum):
+    """
+        return codes are a fixed size coming back in
+    """
+    OK = "OK_"
+    ERROR = "ERR"
+    RANGE = "RNG"
+
+
+"""
+
+
+"""
+
+
 class Sensors():
     """
         Sensor super class to handle returns of object identity
+        Sensors have the following attributes that we need to keep track of:
+            is it powered on
+            what is the supplied power
+
 
 
     """
@@ -25,6 +53,11 @@ class Sensors():
                 io:         define whether sensor is on an input or an output
                 Channel:    which channel is the sensor on, in real world usecase this would likely be a gpio pin or more specific hardware address
         """
+
+        # default state of the sensors that are agnostic to what type the sensor is
+        self.sensorSate = {"Enabled": 0,
+                           "SuppliedPower": 0,
+                           }
         self.sensorType = typeEnum
         self.IO = io
         self.channel = channel
@@ -68,22 +101,44 @@ class SensorOutput(Sensors):
 
 
 class Module():
-    def __init__(self):
+    def SensorFactory(self, Dict):
+        for key, val in Dict:
+            print(key, val)
+
+    def __init__(self, sensorDict):
+        self.SensorFactory(sensorDict)
+
+    def ResponseGenerator(self):
+
         pass
 
-    def SensorFactory(self):
-        pass
+
+# x = SensorInput(SensorType.DIGI, IO.INP, 1)
+# print(x.channel)
 
 
-x = SensorInput(SensorType.DIGI, IO.INP, 1)
-print(x.channel)
-
-
-sensoreDict = {
-    Digi0: 0,
-    Digi1: 1,
-    Ana0: 2,
-    Ana1: 3,
+sensorDict = {
+    "Digi0": 0,
+    "Digi1": 1,
+    "Ana0": 2,
+    "Ana1": 3,
 
 
 }
+
+
+class ProtocolHandler():
+    def __init__(self):
+        pass
+
+    def ReadInput(self):
+        inp = input("> ")
+        return inp
+
+    def ProcessInput(self, inp):
+        pattern = r"\^O\s\w+\s\\n"
+
+        if re.fullmatch(pattern, inp):
+            print(inp)
+        else:
+            return False
